0221b4384db938a618546954d29bfc50476a3a8d
Daniel Jackoway <commits@danieljackoway.com>
2021-12-23 10:14:16 -0500

    subtraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,30 @@
 use std::collections::LinkedList;
 
+fn do_operation(token: &str, stack: &mut LinkedList<i32>) -> () {
+    let right = stack.pop_back().unwrap();
+    let left = stack.pop_back().unwrap();
+    let result = match token {
+        "+" => left + right,
+        "-" => left - right,
+        _ => panic!("unsupported operand")
+    };
+    stack.push_back(result);
+}
+
 pub fn rpn(m: &str) -> i32 {
     let mut stack: LinkedList<i32> = LinkedList::new();
     for token in m.split_ascii_whitespace() {
+        match token {
+            "+" | "-" => do_operation(token, &mut stack),
+            _ => stack.push_back(token.parse().unwrap()),
+        }
         if token == "+" {
-            let right = stack.pop_back().unwrap();
-            let left = stack.pop_back().unwrap();
-            stack.push_back(left + right);
         } else {
-            stack.push_back(token.parse().unwrap());
+            
         }
     }
     assert_eq!(stack.len(), 1);
-    return *stack.front().unwrap();
+    *stack.front().unwrap()
 }
 
 
@@ -33,5 +45,10 @@ mod tests {
     #[test]
     fn basic_addition() {
         assert_eq!(5, rpn("2 3 +"))
+    }
+
+    #[test]
+    fn basic_subtract() {
+        assert_eq!(-1, rpn("2 3 -"))
     }
 }
