a2be32eb5f60bae4ce96e2fe9763c3afdbd846ad
Daniel Jackoway <commits@danieljackoway.com>
2021-12-23 09:28:40 -0500

    addition

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,13 @@ use std::collections::LinkedList;
 pub fn rpn(m: &str) -> i32 {
     let mut stack: LinkedList<i32> = LinkedList::new();
     for token in m.split_ascii_whitespace() {
-        stack.push_back(token.parse().unwrap());
+        if token == "+" {
+            let right = stack.pop_back().unwrap();
+            let left = stack.pop_back().unwrap();
+            stack.push_back(left + right);
+        } else {
+            stack.push_back(token.parse().unwrap());
+        }
     }
     assert_eq!(stack.len(), 1);
     return *stack.front().unwrap();
@@ -22,5 +28,10 @@ mod tests {
     #[test]
     fn rpn_trivial() {
         assert_eq!(3, rpn("3"))
+    }
+
+    #[test]
+    fn basic_addition() {
+        assert_eq!(5, rpn("2 3 +"))
     }
 }
